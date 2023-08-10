1efd0e0d2b1eb7f5bc08d0f9fe12e2ccb7d515be
moham <Mohamed Alaraby>
2023-08-10 20:15:28 +0300

    mod1

--- lib/presentation/my_screens/home_screen.dart
+++ lib/presentation/my_screens/home_screen.dart
@@ -26,6 +26,7 @@ class HomeScreen extends StatelessWidget {
         builder: (context, state) {
           return state is FamiliesGetDetailedFamiliesSuccess
               ? ListView.separated(
+                  shrinkWrap: true,
                   itemBuilder: (context, index) {
                     return _buildListItem(
                         index, context, cubit.detailedFamilies[index]);
@@ -69,25 +70,28 @@ class HomeScreen extends StatelessWidget {
         child: Row(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Container(
-              height: MediaQuery.of(context).size.height * 0.14,
-              padding: const EdgeInsets.symmetric(horizontal: 8),
-              decoration: BoxDecoration(
-                color: Colors.green,
-                borderRadius: const BorderRadius.only(
-                  bottomRight: Radius.circular(16),
-                  topRight: Radius.circular(16),
+            Expanded(
+              flex: 1,
+              child: Container(
+                height: MediaQuery.of(context).size.height * 0.14,
+                padding: const EdgeInsets.symmetric(horizontal: 8),
+                decoration: BoxDecoration(
+                  color: Colors.green,
+                  borderRadius: const BorderRadius.only(
+                    bottomRight: Radius.circular(16),
+                    topRight: Radius.circular(16),
+                  ),
+                  shape: BoxShape.rectangle,
+                  border: Border.all(color: Colors.transparent, width: 2.0),
                 ),
-                shape: BoxShape.rectangle,
-                border: Border.all(color: Colors.transparent, width: 2.0),
-              ),
-              child: Center(
-                child: Text(
-                  '${S.of(context).family}${index + 1}',
-                  style: const TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
+                child: Center(
+                  child: Text(
+                    '${S.of(context).family}${index + 1}',
+                    style: const TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
                   ),
                 ),
               ),
@@ -95,40 +99,43 @@ class HomeScreen extends StatelessWidget {
             const SizedBox(
               width: 8,
             ),
-            Padding(
-              padding:
-                  const EdgeInsetsDirectional.only(start: 8, top: 8, bottom: 8),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    '${S.of(context).name} /${family.husband?.name} ',
-                    style: const TextStyle(
-                      fontSize: 12,
-                      fontWeight: FontWeight.normal,
-                      color: Colors.black,
+            Expanded(
+              flex: 3,
+              child: Padding(
+                padding: const EdgeInsetsDirectional.only(
+                    start: 8, top: 8, bottom: 8),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      '${S.of(context).name} /${family.husband?.name} ',
+                      style: const TextStyle(
+                        fontSize: 12,
+                        fontWeight: FontWeight.normal,
+                        color: Colors.black,
+                      ),
                     ),
-                  ),
-                  Text(
-                    '${S.of(context).address} / ',
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                    style: const TextStyle(
-                      fontSize: 12,
-                      fontWeight: FontWeight.normal,
-                      color: Colors.black,
+                    Text(
+                      '${S.of(context).address} / ${family.familyInfo?.familyAdress}',
+                      maxLines: 1,
+                      overflow: TextOverflow.clip,
+                      style: const TextStyle(
+                        fontSize: 12,
+                        fontWeight: FontWeight.normal,
+                        color: Colors.black,
+                      ),
                     ),
-                  ),
-                  Text(
-                    '${S.of(context).phone} / ',
-                    style: const TextStyle(
-                      fontSize: 12,
-                      fontWeight: FontWeight.normal,
-                      color: Colors.black,
+                    Text(
+                      '${S.of(context).phone} / ${family.husband?.teleNumber}',
+                      style: const TextStyle(
+                        fontSize: 12,
+                        fontWeight: FontWeight.normal,
+                        color: Colors.black,
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
           ],
